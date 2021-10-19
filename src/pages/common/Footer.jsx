10fedc620808bc600015ae2f5e0bd29e22989c07
RathodDeven <devenrathodrd@gmail.com>
2021-10-19 12:20:55 +0530

    added me and common footer with styles

---
+++ src/pages/common/Footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Logo from '../../asset/images/logo.png'
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <>
+        <div className="footer-dark">
+                <footer>
+                    <div className="container">
+                        <div className="row">
+                            <div className="footer-container">
+                            <div>
+                                <img src={Logo} width="300px" height="100px" alt="logo" className="logo" />
+                            </div>
+                            <div>
+                                <h3>Pages</h3>
+                                <ul>
+                                    <li><Link to="/">Home</Link></li>
+                                    <li><Link to="/market">Market</Link></li>
+                                    <li><Link to="/about">About</Link></li>
+                                    <li><Link to="/me">Me</Link></li>
+                                    
+                                </ul>
+                            </div>
+                            <div>
+                                <h3>Support</h3>
+                                <ul>
+                                    <li><Link to="/faq">FAQ</Link></li>
+                                    <li><Link to="/forum">Forum</Link></li>
+                                    <li><Link to="/contact us">Contact Us</Link></li>
+                                </ul>
+                            </div>
+                            </div>
+                            <br />
+                            <br />
+                            <div className="col item social"><a href="#"><i className="fa fa-facebook"></i></a><a href="#"><i className="fa fa-twitter"></i></a><a href="#"><i className="fa fa-youtube"></i></a><a href="#"><i className="fa fa-instagram"></i></a><a href="#"><i className="fa fa-google"></i></a></div>
+                        </div>
+                        
+                        <p className="copyright">interality.io © 2021</p>
+                    </div>
+                </footer>
+            </div>
+
+            
+        </>
+    )
+}
+
+export default Footer
--- src/pages/me/Me.jsx
+++ src/pages/me/Me.jsx
@@ -1,6 +1,83 @@
+import './style.css'
+import Footer from '../common/Footer';
+
 const Me = () => {
+    // const iframeUrl = 'https://demo.readyplayer.me/'
+
+    // Listen to messages from the iframe
+    window.addEventListener('message', receiveMessage, false)
+
+    function receiveMessage(event){
+        console.log(event.data);
+    }
+
+    //   // Handle messages from the iframe
+    //   function receiveMessage(event) {
+    //     // Get URL to avatar
+    //     const url = event.data
+    //     console.log(`Avatar URL: ${url}`)
+    //     document.querySelector('#avatarUrl').innerHTML = `Avatar URL: ${url}`
+    //     document.querySelector('.container').removeChild(iframe)
+    //   }
+
+    //   function loadIframe() {
+    //     let iframe = document.getElementById('iframe')
+
+    //     if (!iframe) {
+    //       iframe = document.createElement('iframe')
+    //       document.querySelector('.container').appendChild(iframe)
+    //     }
+    //     iframe.id = 'iframe'
+    //     iframe.src = iframeUrl
+    //     iframe.className = 'content'
+    //     iframe.allow = 'camera *; microphone *'
+    //   }
     return (
-        <h1>Me Page</h1>
+        <>
+            <div className="flex-container">
+                <div className="card">
+                    <img src="https://www.w3schools.com/howto/img_avatar.png" alt="John" className="profile_img" ></img>
+                    <h1>John Doe</h1>
+                    <p> johndoe@gmail.com</p>
+                    <br />
+                    <p className="title">CEO & Founder, Example</p>
+                    <br />
+                    <a href="#"><i className="fa fa-dribbble"></i> </a>
+                    <a href="#"><i className="fa fa-twitter"></i> </a>
+                    <a href="#"><i className="fa fa-linkedin"></i> </a>
+                    <a href="#"><i className="fa fa-facebook"></i> </a>
+                    <br />
+                    <p><button>Contact</button></p>
+                    
+                </div>
+
+                <div className="container">
+                <iframe id="iframe"
+                    title="Inline Frame Example"
+                    width="500"
+                    height="900"
+                    src="https://interality.readyplayer.me/"
+                    className="content"
+                    allow="camera *; microphone *">
+                        
+                </iframe>
+                </div>
+            </div>
+
+            <div className="img_container">
+            <img src="https://ik.imagekit.io/ikmedia/accessories_banner.jpg?tr=w-1200,h-400" height="300px" alt="Snow" className="profile_img" />
+            
+            <div className="top-right">
+                <h1 style={{"color":"black","fontSize":"35px"}}>Join Our Community</h1>
+                <p style={{"color":"black","fontSize":"15px"}}>Be part of our amazing community and stay
+    tuned for the news.</p>
+                <button className="join-btn"style={{"fontSize":"20px"}} onClick={ () => {
+                    window.location.href = 'https://www.interality.io/'
+                }} >Join Us</button>
+            </div>
+            </div>
+            <Footer />
+                        </>
     )
 }
 export default Me;
